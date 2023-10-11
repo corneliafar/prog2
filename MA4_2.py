81b557e66b994a37ed5759d9bacc19fdd2e1f41f
Cornelia Fardig <cofa3943@arrhenius.it.uu.se>
2023-10-11 21:54:12 +0200

    all files

--- MA4_2.py
+++ MA4_2.py
@@ -7,17 +7,66 @@ import matplotlib.pyplot as plt
 import math
 import concurrent.futures as future
 
+from numba import njit
 from time import perf_counter as pc
 from time import sleep as pause
 
-def main():
+"""def main():
 	f = Person(5)
 	print(f.get())
 	f.set(7)
-	print(f.get())
+	print(f.get())"""
 
-	n = 10
-	print(f'Fibonacci of {n} is: {f.fib(n)}')
+def fib_py(n): 		# recursively calculates fibonacci sequence for n
+	if n <= 1:
+		return n
+	else:
+		return fib_py(n-1) + fib_py(n-2)
+
+@njit
+def fib_numba(n):	# same as fib_py but using numba decorator
+	if n <= 1:	# to speed up compilation
+		return n
+	else:
+		return fib_numba(n-1) + fib_numba(n-2)
+
+# ---------------------------------------------------
+def fib_timings(n):
+	timings = {}
+
+	"""start = pc()
+	fib_py(n)
+	timings['python'] = pc() - start"""
+
+	start = pc()
+	fib_numba(n)
+	timings['numba'] = pc() - start
+
+	f = Person(n)
+	start = pc()
+	f.fib(n)
+	timings['cpp'] = pc() - start
+
+	return timings
+
+def parallell_fib(n_values):
+	with future.ProcessPoolExecutor() as ex:
+        	results = list(ex.map(fib_timings, n_values))
+	return results
+
+def plot_fib(n_values, results, filename):
+	fig, ax = plt.subplots()
+
+	for method in ['numba', 'cpp']:
+		ax.plot(n_values, [res[method] for res in results], label=method)
+
+	ax.set_xlabel('n')
+	ax.set_ylabel('Time (seconds)')
+	ax.legend()
+	plt.savefig(filename)
+
+
+# -------------------------------------------------
 
 # ---------- 1.1 ----------
 def monte_carlo(n):
@@ -36,7 +85,7 @@ def monte_carlo(n):
 		else:
 			x_outside.append(x)
 			y_outside.append(y)
-	
+
 	pi_approx = 4 * inside_circle / n
 
 	return pi_approx, x_inside, y_inside, x_outside, y_outside
@@ -78,8 +127,49 @@ def monte_carlo_parallell(n, d, p):
 		return sum(results) / p
 
 
+
 if __name__ == '__main__':
-	main()
+	# main()
+
+	# ---------- 2.2 ----------
+	"""time_py = 0
+	time_numba = 0
+	time_cpp = 0
+
+	for n in range(30,46): # 30 -> 45
+		# python version
+		start = pc()
+		result_py = fib_py(n)
+		end = pc()
+		time_py += (end-start)
+
+		# numba version
+		start = pc()
+		result_numba = fib_numba(n)
+		end = pc()
+		time_numba += (end-start)
+
+		# c++ version
+		f = Person(n)
+		start = pc()
+		result_cpp = f.fib(n)
+		end = pc()
+		time_cpp += (end-start)
+
+
+	print('----- PART 2.2 -----')
+	#print(f"Total time for fib_py: {round(time_py, 2)} seconds.")
+	#print(f"Total time for fib_numba: {round(time_numba, 2)} seconds.")
+	print(f"Total time for Person.fib: {round(time_cpp, 2)} seconds.")"""
+
+	# numba warm-up
+	fib_numba(10)
+
+	n_values = list(range(30,48))
+	results = parallell_fib(n_values)
+	plot_fib(n_values, results, 'fib_30_to_47_numba_cpp.png')
+
+	print()
 
 	# ---------- 1.1 ----------
 	n_values = [50, 100, 200, 400] # n is the amount of random points to generate
@@ -88,7 +178,7 @@ if __name__ == '__main__':
 	# ---------- 1.2 ----------
 	n, d1, d2 = 100000, 2, 11
 
-	print('----- PART 1.2 -----')
+	"""print('----- PART 1.2 -----')
 	start = pc()
 	approximation_1 = monte_carlo_volume(n, d1)
 	end = pc()
@@ -104,10 +194,10 @@ if __name__ == '__main__':
 	exact_value_2 = math.pi**(d2/2) / (math.gamma((d2/2) + 1))
 	print(f'Exact volume of {d2}-dimensional hypersphere acc. to gamma equation: {exact_value_2}')
 
-	print()
+	print()"""
 
 	# ---------- 1.3 ----------
-	n1, n2, d, p = 1000000, 100000, 11, 10
+	"""n1, n2, d, p = 1000000, 100000, 11, 10
 
 	# print values for sequential and parallell programming as well as theoretical value for hypersphere volume
 	print('----- PART 1.3 -----')
@@ -122,4 +212,4 @@ if __name__ == '__main__':
 	print(f"Approximated volume of {d}-dimensional hypersphere (parallell): {approximation_parallell}. Time: {round(end-start, 2)} seconds.")
 
 	exact_value = math.pi**(d/2) / (math.gamma((d/2) + 1))
-	print(f'Exact volume of {d}-dimensional hypersphere (gamma equation): {exact_value}.')
+	print(f'Exact volume of {d}-dimensional hypersphere (gamma equation): {exact_value}.')"""
