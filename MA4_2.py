1dcdf7730b70de3d2f52b9e280d78702523d8455
corneliafar <147610125+corneliafar@users.noreply.github.com>
2023-10-12 10:06:45 +0200

    Update MA4_2.py

--- MA4_2.py
+++ MA4_2.py
@@ -17,6 +17,7 @@ from time import sleep as pause
 	f.set(7)
 	print(f.get())"""
 
+# ---------- 2.2 ----------
 def fib_py(n): 		# recursively calculates fibonacci sequence for n
 	if n <= 1:
 		return n
@@ -30,43 +31,39 @@ def fib_numba(n):	# same as fib_py but using numba decorator
 	else:
 		return fib_numba(n-1) + fib_numba(n-2)
 
-# ---------------------------------------------------
 def fib_timings(n):
-	timings = {}
+	timings = {}	# creates an empty dictionary
 
-	"""start = pc()
+	start = pc()	# starts time rec
 	fib_py(n)
-	timings['python'] = pc() - start"""
-
+	timings['python'] = pc() - start	# takes current time subtracted with start time and 
+						# stores with the var name 'python' in dict.
 	start = pc()
 	fib_numba(n)
-	timings['numba'] = pc() - start
+	timings['numba'] = pc() - start		# same as above but for numba
 
 	f = Person(n)
 	start = pc()
 	f.fib(n)
-	timings['cpp'] = pc() - start
+	timings['cpp'] = pc() - start		# same as above but for cpp version
 
 	return timings
 
-def parallell_fib(n_values):
+def parallell_fib(n_values):		# run fib parallell to speed up process
 	with future.ProcessPoolExecutor() as ex:
-        	results = list(ex.map(fib_timings, n_values))
-	return results
+        	results = list(ex.map(fib_timings, n_values))	# creates a list that maps together timings and n_values
+	return results						# distributes workload for different n-values
 
-def plot_fib(n_values, results, filename):
-	fig, ax = plt.subplots()
+def plot_fib(n_values, results, filename):	# accepts input for n, fib results and name for the saved file
+	fig, ax = plt.subplots()		# sets the same figure and axis for all the subplots
 
-	for method in ['numba', 'cpp']:
-		ax.plot(n_values, [res[method] for res in results], label=method)
-
-	ax.set_xlabel('n')
-	ax.set_ylabel('Time (seconds)')
-	ax.legend()
-	plt.savefig(filename)
-
-
-# -------------------------------------------------
+	for i in ['python', 'numba', 'cpp']:	# iterates over timings dict from result list
+		ax.plot(n_values, [_[i] for _ in results], label=i)	# extract all result values for each timings dict
+									# i.e. time and each fib value calculation
+	ax.set_xlabel('n')			# x-label = n
+	ax.set_ylabel('Time (seconds)')		# y-label = seconds
+	ax.legend()				# enables a legend
+	plt.savefig(filename)			# ensures fig is saved
 
 # ---------- 1.1 ----------
 def monte_carlo(n):
@@ -76,109 +73,91 @@ def monte_carlo(n):
 	x_outside = []
 	y_outside = []
 
-	for i in range(n):
-		x, y = random.uniform(-1, 1), random.uniform(-1, 1)
-		if x**2 + y**2 <= 1:
-			inside_circle += 1
-			x_inside.append(x)
-			y_inside.append(y)
-		else:
-			x_outside.append(x)
-			y_outside.append(y)
+	for i in range(n):	# iterates over n values
+		x, y = random.uniform(-1, 1), random.uniform(-1, 1)	# create randomised x,y coordinates
+		if x**2 + y**2 <= 1:		# check if point is within theoretical bound
+			inside_circle += 1	# if it is, add +1 to variable inside_circle
+			x_inside.append(x)	# store x-coordinate to inside list
+			y_inside.append(y)	# store y-coordinate to inside list
+		else:				# if not in bounds
+			x_outside.append(x)	# store x-coordinate to outside list
+			y_outside.append(y)	# store y-coordinate to outside list
 
-	pi_approx = 4 * inside_circle / n
+	pi_approx = 4 * inside_circle / n	# 2-d space gives approx 2^2*(inside circ / n)
 
-	return pi_approx, x_inside, y_inside, x_outside, y_outside
+	return pi_approx, x_inside, y_inside, x_outside, y_outside	# return approximation and all lists
 
-def plot(n_values):
-	plt.figure(figsize=(10, 10))
+def plot(n_values, filename):
+	plt.figure(figsize=(10, 10))	# creates a large figure space
 
-	for i, n in enumerate(n_values, 1):
-		pi_approx, x_in, y_in, x_out, y_out = monte_carlo(n)
-		plt.subplot(2, 2, i)  # 2x2 grid for 4 plots
-		plt.scatter(x_in, y_in, c='red', s=10, marker='o')
-		plt.scatter(x_out, y_out, c='blue', s=10, marker='o')
-		plt.title(f"Monte Carlo for n={n}\nπ ≈ {pi_approx:.4f}")
-		plt.xlim(-1, 1)
+	for i, n in enumerate(n_values, 1):	# for every value in n_values (starting from 1)
+		pi_approx, x_in, y_in, x_out, y_out = monte_carlo(n)	# calls monte_carlo for each n-value and stores returned result
+		plt.subplot(2, 2, i)  # creates a 2x2 grid for 4 plots
+		plt.scatter(x_in, y_in, c='red', s=10, marker='o')	# plots values inside bounds with red circles
+		plt.scatter(x_out, y_out, c='blue', s=10, marker='o')	# plots values outside bounds with blue circles
+		plt.title(f"Monte Carlo for n={n}\nπ ≈ {pi_approx:.4f}")	# title for plots containing pi approximation
+		plt.xlim(-1, 1)		# sets plot axis limits
 		plt.ylim(-1, 1)
-		plt.grid(True)
+		plt.grid(True)		# enables a grid
 
-	plt.tight_layout()
+	plt.tight_layout()	# tight layout for better fit to make the plot easier to read
+	plt.savefig(filename)
 	plt.show()
 
 # ---------- 1.2 ----------
 # calculate which coordinates are within bounds in accordance to sum(x1^2+x2^2+ ... +xn^2) < 1
-def within_bounds(p):
+def within_bounds(p):	# function that defines what is within bounds in accordance to assignment
 	return sum(x**2 for x in p) <= 1
 
-def monte_carlo_volume(n, d):
+def monte_carlo_volume(n, d):	# n values and d dimensions
+	# lambda function creates a list (through list comprehension) 
+	# of d random points
+	# map connects each d points with each value of n creating
+	# n random points in the d dimensional space
 	random_pt = map(lambda i : [random.uniform(-1, 1) for i in range(d)], range(n))
+	# filter method connects the random points to what values are 
+	# accepted as "within bounds" by calling the within_bounds
+	# function for each value of random_pt. it then stores the
+	# within bounds values to the variable inside_circ
 	inside_circ = filter(within_bounds, random_pt)
+	# calculates approximated volume of the hypersphere for d dimensions
 	approximation = 2**d * (sum(1 for i in inside_circ) / n)
 	return approximation
 
 # ---------- 1.3 ----------
 def monte_carlo_parallell(n, d, p):
-	with future.ProcessPoolExecutor (max_workers = p) as ex:
-		# submit(function, argument, additional argument). // = integer division
+	with future.ProcessPoolExecutor (max_workers = p) as ex:	# max workers is p
+		# syntax: submit(function, argument, additional argument). // = integer division
+		# list comprehension that creates a list by executing submit p times (from i=0 to i=p-1)
+		# this returns future object that are stored into the list
 		fs = [ex.submit(monte_carlo_volume, n, d) for i in range(p)]
 		# result() waits and collects future results
 		results = [i.result() for i in future.as_completed(fs)]
 		return sum(results) / p
 
 
-
 if __name__ == '__main__':
 	# main()
-
+	
 	# ---------- 2.2 ----------
-	"""time_py = 0
-	time_numba = 0
-	time_cpp = 0
-
-	for n in range(30,46): # 30 -> 45
-		# python version
-		start = pc()
-		result_py = fib_py(n)
-		end = pc()
-		time_py += (end-start)
-
-		# numba version
-		start = pc()
-		result_numba = fib_numba(n)
-		end = pc()
-		time_numba += (end-start)
-
-		# c++ version
-		f = Person(n)
-		start = pc()
-		result_cpp = f.fib(n)
-		end = pc()
-		time_cpp += (end-start)
-
-
-	print('----- PART 2.2 -----')
-	#print(f"Total time for fib_py: {round(time_py, 2)} seconds.")
-	#print(f"Total time for fib_numba: {round(time_numba, 2)} seconds.")
-	print(f"Total time for Person.fib: {round(time_cpp, 2)} seconds.")"""
-
-	# numba warm-up
+	# numba warm-up, in accordance to recommendations online
 	fib_numba(10)
 
-	n_values = list(range(30,48))
+	n_values = list(range(28,44))
 	results = parallell_fib(n_values)
-	plot_fib(n_values, results, 'fib_30_to_47_numba_cpp.png')
+	plot_fib(n_values, results, 'fib_28_to_43.png')
 
 	print()
 
 	# ---------- 1.1 ----------
 	n_values = [50, 100, 200, 400] # n is the amount of random points to generate
+	plot(n_values, 'monte_carlo.png')
 	#plot(n_values)
 
 	# ---------- 1.2 ----------
 	n, d1, d2 = 100000, 2, 11
 
-	"""print('----- PART 1.2 -----')
+	print('----- PART 1.2 -----')
 	start = pc()
 	approximation_1 = monte_carlo_volume(n, d1)
 	end = pc()
@@ -194,10 +173,10 @@ if __name__ == '__main__':
 	exact_value_2 = math.pi**(d2/2) / (math.gamma((d2/2) + 1))
 	print(f'Exact volume of {d2}-dimensional hypersphere acc. to gamma equation: {exact_value_2}')
 
-	print()"""
+	print()
 
 	# ---------- 1.3 ----------
-	"""n1, n2, d, p = 1000000, 100000, 11, 10
+	n1, n2, d, p = 1000000, 100000, 11, 10
 
 	# print values for sequential and parallell programming as well as theoretical value for hypersphere volume
 	print('----- PART 1.3 -----')
@@ -212,4 +191,4 @@ if __name__ == '__main__':
 	print(f"Approximated volume of {d}-dimensional hypersphere (parallell): {approximation_parallell}. Time: {round(end-start, 2)} seconds.")
 
 	exact_value = math.pi**(d/2) / (math.gamma((d/2) + 1))
-	print(f'Exact volume of {d}-dimensional hypersphere (gamma equation): {exact_value}.')"""
+	print(f'Exact volume of {d}-dimensional hypersphere (gamma equation): {exact_value}.')
